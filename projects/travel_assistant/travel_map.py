23209e234cd4d7dcdf13697c87feacf580540a5e
Muhammad Maaz Uddin <39926764+maaznadeem246@users.noreply.github.com>
2024-02-06 16:28:38 +0000

    latset work

--- projects/travel_assistant/travel_map.py
+++ projects/travel_assistant/travel_map.py
@@ -1,7 +1,13 @@
 from typing import TypedDict
 import os
+from dotenv import load_dotenv, find_dotenv # type: ignore
 import plotly.graph_objects as go # type: ignore
+
+
+_ : bool = load_dotenv(find_dotenv())
+
 MAPBOX_TOKEN = os.environ.get("MAPBOX_TOKEN")
+
 
 
 
@@ -14,30 +20,36 @@ class TravelMap:
     def __init__(self) -> None:
         self.mainLocation : locationType = {'name':'','lat':0,'lon':0}
         self.suggestLocations : suggestPlaces = []
+        
         lat = [sl['lat'] for sl in self.suggestLocations if "lat" in sl]
         lon = [sl['lon'] for sl in self.suggestLocations if "lon" in sl]
         name = [sl['name'] for sl in self.suggestLocations if 'name' in sl]
 
-        self.fig = go.Figure(
-            go.Scattermapbox(
+        self.fig = go.Figure(go.Scattermapbox(
+            # df=mapData,
             mode = "markers",
-            lat=lat,
-            lon=lon,
+            lon = lon, lat = lat,
             hovertext=name,
             marker=dict(
                 color='red',
                 size=10,
+            
             ),
-            textposition = "bottom right")
+            textposition = "bottom right")).update_layout(
+            mapbox_accesstoken=MAPBOX_TOKEN,
+            
+            showlegend = False,
+            margin={"r":0,"t":0,"l":0,"b":0}
         )
 
 
-        self.fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-        self.fig.update_traces(marker_color='red', selector=dict(type='scattermapbox'))
+
+
  
     def showMap(self) -> None:
         # self.mainLocation = mainLocation
         # self.suggestLocations = suggestLocations
+
         self.fig.show()
 
     def updateMap(self,mainLocation:locationType,suggestLocations:suggestPlaces)->None:
@@ -47,27 +59,22 @@ class TravelMap:
         if mainLocation != None : self.mainLocation = mainLocation
         if suggestLocations != None : self.suggestLocations = suggestLocations
 
-        if bool(len(self.suggestLocations)) : self.suggestLocations.insert(0,mainLocation)
         
         print(self.mainLocation)
         print(self.suggestLocations)
         lat = [sl['lat'] for sl in self.suggestLocations if "lat" in sl]
         lon = [sl['lon'] for sl in self.suggestLocations if "lon" in sl]
         name = [sl['name'] for sl in self.suggestLocations if 'name' in sl]
-        print('test')
-        print(lat)
-        print(lon)
-        print(name)
-        # self.fig.update_traces(lat=lat,lon=lon,hovertext=name)
-
-        self.fig.update_layout( 
-        mapbox = {
-            'zoom':15,
-            'accesstoken': MAPBOX_TOKEN,
-            'style': "outdoors",
-            "center":{'lat': self.mainLocation['lat'], 'lon': self.mainLocation['lon']}   
+        self.fig.update_layout(
+            mapbox = {
+                'zoom':10,
+                'style': "outdoors",
+                "center":{'lat': self.mainLocation['lat'], 'lon': self.mainLocation['lon']}
             },
-        showlegend = False)
+        )
+        self.fig.update_traces(lat=lat,lon=lon,hovertext=name,marker_color='red', selector=dict(type='scattermapbox'))
+
+
 
         
  
